c86e27ce9aed4feba90f7830c3e009ee12834d18
unknown <ishimwefrank0014@gmail.com>
2026-04-20 17:36:25 +0200

    updated the booking steps

---
+++ frontend/src/lib/propertyGallery.js
@@ -0,0 +1,55 @@
+/**
+ * Additional on-property photography for the home page gallery strip.
+ */
+
+import studioSpace from '../images/2.jpeg';
+import loungeArea from '../images/4.jpeg';
+import bedroomStay from '../images/5.jpeg';
+import bedroomWorkspace from '../images/6.jpeg';
+import interiorFlooring from '../images/7.jpeg';
+import gymFloor from '../images/8.jpeg';
+import gymEquipment from '../images/9.jpeg';
+import salonSide from '../images/nziza side salon.jpeg';
+
+export const PROPERTY_GALLERY_ITEMS = [
+  {
+    src: studioSpace,
+    alt: 'Bright studio and training space at Nziza House',
+    caption: 'Studio space',
+  },
+  {
+    src: loungeArea,
+    alt: 'Comfortable lounge seating at Nziza House',
+    caption: 'Resident lounge',
+  },
+  {
+    src: bedroomStay,
+    alt: 'Guest bedroom with refined finishes',
+    caption: 'Guest suites',
+  },
+  {
+    src: bedroomWorkspace,
+    alt: 'Bedroom with workspace nook',
+    caption: 'Stay & work',
+  },
+  {
+    src: interiorFlooring,
+    alt: 'Interior corridor with rich wood flooring',
+    caption: 'Throughout the property',
+  },
+  {
+    src: gymFloor,
+    alt: 'Fitness floor with treadmills at Nziza House',
+    caption: 'Cardio deck',
+  },
+  {
+    src: gymEquipment,
+    alt: 'Strength and conditioning equipment',
+    caption: 'Training floor',
+  },
+  {
+    src: salonSide,
+    alt: 'Side view of the salon seating area',
+    caption: 'Salon',
+  },
+];
